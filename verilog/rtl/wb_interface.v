05d7de6d9016da64cac8a607f5bc3854dae23204
Wouter van Verre <woutervanverre@gmail.com>
2022-03-09 16:52:53 +0000

    Design passes simulation (wb_port) now
    Signed-off-by: Wouter van Verre <woutervanverre@gmail.com>

--- verilog/rtl/wb_interface.v
+++ verilog/rtl/wb_interface.v
@@ -9,7 +9,7 @@ module wb_interface #
     parameter SEQ_WIDTH = 8,
     parameter E_WIDTH = 16,
     
-    parameter PARALLEL_UNITS = 4
+    parameter PARALLEL_UNITS = 2
 )
 (
     input  wire                                 wb_clk_i,
@@ -25,7 +25,7 @@ module wb_interface #
 
     output wire                                 o_rst,
 
-    input wire [PARALLEL_UNITS*SEQ_WIDTH-1:0]   i_seq,
+    input wire [PARALLEL_UNITS*72-1:0]          i_seq,
     input wire [PARALLEL_UNITS*E_WIDTH-1:0]     i_e,
     input wire [PARALLEL_UNITS-1:0]             i_done
 );
@@ -34,14 +34,8 @@ localparam N_REGISTERS = 8*PARALLEL_UNITS;
 localparam REG_WIDTH = $clog2(N_REGISTERS);
 
 wire [REG_WIDTH-1:0] aword;
-wire amatch;
 reg [31:0] cfg_reg;
-  
-reg [$clog2(PARALLEL_UNITS*8)-1:0] temp;
-reg [$clog2(PARALLEL_UNITS)-1:0] seq_idx;
 
-assign amatch = (wbs_adr_i & (32'h FF00_0000)) == BASE_ADR;
-assign aword = wbs_adr_i[REG_WIDTH+2-1:2];
 assign o_rst = cfg_reg[0];
 
 always @(posedge wb_clk_i)
@@ -51,56 +45,32 @@ always @(posedge wb_clk_i) begin
     if (wb_rst_i) begin
         wbs_dat_o <= 0;
     end else if (wbs_cyc_i & wbs_stb_i & ~wbs_we_i) begin
-        if (!amatch) begin
-            wbs_dat_o <= 0;
-        end else if (aword == 0) begin
+        if (wbs_adr_i == (BASE_ADR | 32'h00)) 
             wbs_dat_o <= {{(32-PARALLEL_UNITS){1'b0}}, i_done};
-        end else if (aword == 1) begin
+        else if (wbs_adr_i == (BASE_ADR | 32'h04)) 
             wbs_dat_o <= cfg_reg;
-        end else if (aword < PARALLEL_UNITS) begin
-            wbs_dat_o <= 0;
-        end else if (aword < 2*PARALLEL_UNITS) begin
-            wbs_dat_o <= {{(32-E_WIDTH){1'b0}}, i_e[aword-PARALLEL_UNITS]};
-        end else if (aword < 4*PARALLEL_UNITS) begin
-            wbs_dat_o <= 0;
-        end else if (aword < 8*PARALLEL_UNITS) begin          
-            temp = (aword[$clog2(PARALLEL_UNITS*8)-1:0] - 4 * PARALLEL_UNITS) / 4;
-            seq_idx = temp[$clog2(PARALLEL_UNITS)-1:0];
-            
-            if (aword[1:0] == 2'b00) begin
-                
-                if (SEQ_WIDTH < 32) begin
-                    wbs_dat_o <= {{(32-SEQ_WIDTH){1'b0}}, i_seq[seq_idx][SEQ_WIDTH-1:0]};
-                end else begin
-                    wbs_dat_o <= i_seq[seq_idx][31:0];
-                end
 
-            end else if (aword[1:0] == 2'b01) begin
+        else if (wbs_adr_i == (BASE_ADR | 32'h08)) 
+            wbs_dat_o <= {{(32-E_WIDTH){1'b0}}, i_e[E_WIDTH-1:0]};
+        else if (wbs_adr_i == (BASE_ADR | 32'h0C)) 
+            wbs_dat_o <= {{(32-E_WIDTH){1'b0}}, i_e[2*E_WIDTH-1:E_WIDTH]};
 
-                if (SEQ_WIDTH <= 32) begin
-                    wbs_dat_o <= 0;
-                end else if (SEQ_WIDTH < 63) begin
-                    wbs_dat_o <= {{(64-SEQ_WIDTH){1'b0}}, i_seq[seq_idx][SEQ_WIDTH-1:32]};
-                end else begin
-                    wbs_dat_o <= i_seq[seq_idx][63:32];
-                end
-                
-            end else if (aword[1:0] == 2'b10) begin
+        else if (wbs_adr_i == (BASE_ADR | 32'h10)) 
+            wbs_dat_o <= i_seq[31:0];
+        else if (wbs_adr_i == (BASE_ADR | 32'h14)) 
+            wbs_dat_o <= i_seq[63:32];
+        else if (wbs_adr_i == (BASE_ADR | 32'h18)) 
+            wbs_dat_o <= {24'b0, i_seq[71:64]};
 
-                if (SEQ_WIDTH <= 64) begin
-                    wbs_dat_o <= 0;
-                end else if (SEQ_WIDTH < 95) begin
-                    wbs_dat_o <= {{(96-SEQ_WIDTH){1'b0}}, i_seq[seq_idx][SEQ_WIDTH-1:64]};
-                end else begin
-                    wbs_dat_o <= i_seq[seq_idx][95:64];
-                end
-                
-            end else if (aword[1:0] == 2'b11) begin
-                wbs_dat_o <= 0;
-            end
-        end else begin
+        else if (wbs_adr_i == (BASE_ADR | 32'h20)) 
+            wbs_dat_o <= i_seq[103:72];
+        else if (wbs_adr_i == (BASE_ADR | 32'h24)) 
+            wbs_dat_o <= i_seq[135:104];
+        else if (wbs_adr_i == (BASE_ADR | 32'h28)) 
+            wbs_dat_o <= {24'b0, i_seq[143:136]};
+
+        else 
             wbs_dat_o <= 0;
-        end
     end
 end
 
